0bc7551312671bdfa4ac94da50ddbf1c437a21d5
Brendan Galloway <brendan@flanksource.com>
2021-03-03 10:26:07 +0200

    test: add template testing framework

--- go.mod
+++ go.mod
@@ -3,19 +3,33 @@ module flanksource/template-operator-dbs
 go 1.16
 
 require (
-	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/google/gofuzz v1.2.0 // indirect
-	golang.org/x/net v0.0.0-20210226101413-39120d07d75e // indirect
-	golang.org/x/text v0.3.5 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
-	k8s.io/api v0.20.2 // indirect
-	k8s.io/apimachinery v0.20.4
-	k8s.io/klog/v2 v2.5.0 // indirect
-	sigs.k8s.io/controller-runtime v0.8.2
-	sigs.k8s.io/structured-merge-diff/v4 v4.0.3 // indirect
+	github.com/flanksource/kommons v0.2.2
+	github.com/hashicorp/go-version v1.2.0 // indirect
+	github.com/imdario/mergo v0.3.11 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/prometheus/procfs v0.2.0 // indirect
+	github.com/sirupsen/logrus v1.7.1 // indirect
+	go.etcd.io/etcd v0.5.0-alpha.5.0.20200910180754-dd1b699fc489 // indirect
+	golang.org/x/crypto v0.0.0-20201002170205-7f63de1d35b0 // indirect
+	golang.org/x/mod v0.4.1 // indirect
+	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
+	golang.org/x/tools v0.1.0 // indirect
+	google.golang.org/genproto v0.0.0-20201110150050-8816d57aaa9a // indirect
+	google.golang.org/grpc v1.27.1 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
+	k8s.io/api v0.20.2
+	k8s.io/apimachinery v0.20.2
+	k8s.io/cli-runtime v0.20.1 // indirect
+	k8s.io/utils v0.0.0-20201110183641-67b214c5f920 // indirect
+	sigs.k8s.io/controller-runtime v0.5.7
 )
 
 replace (
+	github.com/docker/docker => github.com/moby/moby v17.12.0-ce-rc1.0.20210128214336-420b1d36250f+incompatible
 	gopkg.in/hairyhenderson/yaml.v2 => github.com/maxaudron/yaml v0.0.0-20190411130442-27c13492fe3c
-	k8s.io/client-go => k8s.io/client-go v0.19.3
+	helm.sh/helm/v3 => helm.sh/helm/v3 v3.5.1
+	k8s.io/api => k8s.io/api v0.19.4
+	k8s.io/client-go => k8s.io/client-go v0.19.4
+	k8s.io/kubectl => k8s.io/kubectl v0.19.4
+	sigs.k8s.io/controller-runtime => sigs.k8s.io/controller-runtime v0.7.2
 )
---
+++ test/run_test.go
@@ -0,0 +1,128 @@
+package test
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/mitchellh/go-homedir"
+	"io/ioutil"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	yamlutil "k8s.io/apimachinery/pkg/util/yaml"
+	"os"
+	"path"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/flanksource/kommons"
+)
+type TestInstance struct {
+	CRD string
+	Template string
+	Fixture string
+}
+
+var TestSpace []TestInstance
+
+func TestMain(m *testing.M) {
+	TestSpace = []TestInstance{
+		{
+			CRD:      "../config/crd/db/db.flanksource.com_elasticsearchdbs.yaml",
+			Template: "../config/templates/elasticsearch-db.yaml",
+			Fixture:  "fixtures/elasticsearch-db.yaml",
+		},
+	}
+	code := m.Run()
+	os.Exit(code)
+}
+
+func TestRunChecks(t *testing.T) {
+	home, err := homedir.Dir()
+	if err != nil {
+		t.Fatalf("Coulf not determine home path: %v", err)
+	}
+	kubefile := path.Join(home, ".kube/config")
+	config, err := ioutil.ReadFile(kubefile)
+	if err != nil {
+		t.Fatalf("Could not open kube config: %v", err)
+	}
+	client, err := kommons.NewClientFromBytes(config)
+	if err != nil {
+		t.Fatalf("Could not create kubernetes client: %v", err)
+	}
+	wg := sync.WaitGroup{}
+	for _, fixture := range TestSpace {
+		wg.Add(1)
+		go func() {
+			runFixture(t, fixture, client)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+}
+
+func ApplyObject(path string, client *kommons.Client) error {
+	var obj *unstructured.Unstructured
+	var err error
+	if err != nil {
+		return err
+	}
+	crd, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+	decoder := yamlutil.NewYAMLOrJSONDecoder(bytes.NewReader([]byte(crd)), 1024)
+
+	for true {
+		err := decoder.Decode(&obj)
+		if err != nil { break }
+		if obj == nil { continue }
+		err = client.ApplyUnstructured(obj.GetNamespace(), obj)
+		if err != nil {
+			return fmt.Errorf("error decoding %s: %s", crd, err)
+		}
+	}
+	return nil
+}
+
+func runFixture(t *testing.T, fixture TestInstance, client *kommons.Client) {
+	t.Run(fixture.Fixture, func(t *testing.T) {
+		t.Log("Applying CRD")
+		if err := ApplyObject(fixture.CRD, client); err != nil {
+			t.Fatal(err)
+		}
+		time.Sleep(time.Second*10)
+		t.Log("Applying Template")
+		if err := ApplyObject(fixture.Template, client); err != nil {
+			t.Fatal(err)
+		}
+		time.Sleep(time.Second*10)
+		t.Log("Applying test instance")
+		if err := ApplyObject(fixture.Fixture, client); err != nil {
+			t.Fatal(err)
+		}
+		time.Sleep(time.Second*10)
+
+		fixture, err := ioutil.ReadFile(fixture.Fixture)
+		if err != nil {
+			t.Fatal(err)
+		}
+		var obj unstructured.Unstructured
+		decoder := yamlutil.NewYAMLOrJSONDecoder(bytes.NewReader([]byte(fixture)), 1024)
+		if err := decoder.Decode(&obj); err != nil {
+			t.Fatalf("Could not unmarshal test instance: %v",err)
+		}
+
+		client, err := client.GetClientset()
+		if err != nil {
+			t.Fatal("Could not retrieve client")
+		}
+		start := time.Now()
+
+		t.Log("Checking test instance output")
+		kommons.WaitForNamespace(client, obj.GetNamespace(), time.Second*300)
+		if start.Add(time.Second*300).Before(time.Now()) {
+			t.Log("Namespace not ready within 5 min")
+			t.Fail()
+		}
+	})
+}
